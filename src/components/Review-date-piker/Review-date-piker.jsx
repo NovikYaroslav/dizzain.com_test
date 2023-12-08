98e548ea1074d9bd716c4b98d790f11f461fa2c2
Novik Yaroslav <yaroslavnowik@yandex.ru>
2023-12-08 20:41:00 +0100

    miu date piker try

--- src/components/Review-date-piker/Review-date-piker.jsx
+++ src/components/Review-date-piker/Review-date-piker.jsx
@@ -1,73 +1,23 @@
 import React, { useState } from 'react';
 import datePikerIcon from '../../img/date-piker.png';
-import { ConfigProvider, DatePicker, Calendar } from 'antd';
-import ruRU from 'antd/es/locale/ru_RU';
-import dayjs from 'dayjs'; // Import dayjs
-import 'dayjs/locale/ru'; // Import the locale for dayjs
+import { DatePicker } from '@mui/x-date-pickers';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import './review-date-piker.css';
 
-dayjs.locale('ru');
-
 export default function ReviewDatePiker() {
-  const [selectedDate, setSelectedDate] = useState();
+  function MuiIcon() {
+    return <img src={datePikerIcon} alt='Date picker opening icon' width={32} />;
+  }
 
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-  };
-
-  const handleRelativeDate = (days) => {
-    const newDate = selectedDate.add(days, 'day');
-    setSelectedDate(newDate);
-  };
-
-  const handleTodayDate = () => {
-    const currentDate = dayjs();
-    setSelectedDate(currentDate);
-  };
-
+  const [pikedDate, setPikerDate] = useState();
   return (
-    <ConfigProvider
-      locale={ruRU}
-      theme={{
-        components: {
-          DatePicker: {
-            cellRangeBorderColor: '#BAB6B4',
-            cellHoverBg: '#A7221F',
-            cellWidth: 29,
-            cellHeight: 29,
-          },
-        },
-        token: {
-          colorPrimary: '#BAB6B4',
-          borderRadius: 12,
-          fontFamily: 'Roboto',
-          boxShadowSecondary: '0px 4px 25px 0px rgba(0, 0, 0, 0.50)',
-          padding: 12,
-        },
-      }}>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DatePicker
-        allowClear={false}
-        format='DD.MM.YYYY'
-        className='review-date-piker'
-        popupClassName='review-date-piker'
-        suffixIcon={<img src={datePikerIcon} alt='календарь' />}
-        showToday={false}
-        value={selectedDate}
-        onChange={handleDateChange}
-        renderExtraFooter={() => (
-          <div className='review-date-piker__footer'>
-            <button onClick={() => handleRelativeDate(-2)} className='review-date-piker__button'>
-              Позавчера
-            </button>
-            <button onClick={() => handleRelativeDate(-1)} className='review-date-piker__button'>
-              Вчера
-            </button>
-            <button onClick={() => handleTodayDate()} className='review-date-piker__button'>
-              Сегодня
-            </button>
-          </div>
-        )}
+        slots={{ openPickerIcon: MuiIcon }}
+        value={pikedDate}
+        onChange={(newValue) => setPikerDate(newValue)}
       />
-    </ConfigProvider>
+    </LocalizationProvider>
   );
 }
